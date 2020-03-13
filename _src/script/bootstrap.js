10802efb5b49fe6114e8fa266f6372aa5e4cc58c
Rick Makkee <rick@mediamonks.com>
2020-03-13 12:03:35 -0700

    Add check for mouse-mode and keyboard-mode to adjust outline styling

--- _src/script/bootstrap.js
+++ _src/script/bootstrap.js
@@ -2,9 +2,24 @@ import waitForStyleSheetsLoaded from './utils/waitForStyleSheetsLoaded';
 import initComponents from './utils/initComponents';
 
 export default function bootstrap(appRoot) {
+  const handleMouseDown = () => {
+    const body = document.querySelector('body');
+
+    body.classList.add('mode-mouse');
+  };
+
+  const handleKeyDown = () => {
+    const body = document.querySelector('body');
+
+    body.classList.remove('mode-mouse');
+  };
+
   document.addEventListener('DOMContentLoaded', () => {
     waitForStyleSheetsLoaded(document).then(() => {
       initComponents(appRoot);
+
+      window.addEventListener('mousedown', handleMouseDown);
+      window.addEventListener('keydown', handleKeyDown);
     });
   });
 }
